d7d81dcc13a2f6fc92bc0a146a5fbbe17172f4f5
Hachikoi-the-creator <facka72@gmail.com>
2023-04-22 20:42:16 -0500

    emmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+/*
+* 1. Make a function that increments the bears amount by 7
+* 2. Make another function that resets the amount of bears
+* 3. Show the amount of bears inside the p tag
+*/
+export default function App() {
+  const [bears, setBears] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Learning useState</h1>
     </div>
-  )
+  );
 }
-
-export default App
