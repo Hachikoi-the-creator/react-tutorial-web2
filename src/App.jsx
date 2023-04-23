262b187474ce029e773fa3617db22a2373d5e18e
Hachikoi-the-creator <facka72@gmail.com>
2023-04-23 15:13:07 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-
-/*
-* 1. Make a function that increments the bears amount by 7
-* 2. Make another function that resets the amount of bears
-* 3. Show the amount of bears inside the p tag
-*/
 export default function App() {
-  const [bears, setBears] = useState(0);
-
   return (
     <div>
-      <h1>Learning useState</h1>
+      <h1>Learning React Hooks</h1>
     </div>
   );
 }
